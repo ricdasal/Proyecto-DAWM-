be31bb2cdc7c6d5895cf1196b978f117cc84f124
Katiuska <kmmarin@espol.edu.ec>
2021-01-07 00:31:51 -0500

    fetch y axio marvel

---
+++ src/js/axioMarvel.js
@@ -0,0 +1,39 @@
+
+
+const STATUS_OK = 200
+const STATUS_CREATED = 201
+const STATUS_NOT_FOUND = 404
+const hash = '48c85474dada2a5b396f3dff77ae5dae';
+const apikey = '7e232c2d12fa937c5f7f46f40a6e52a1';
+const url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=doctor&apikey=${apikey}`;
+  
+btn.addEventListener('click', () => {
+    loadComics()    
+});
+
+const loadComics = async () => {
+    const response = await axios.get(url)
+    switch (response.status){
+        case STATUS_OK:
+            draw(response.data.data.results)
+            break
+        case STATUS_NOT_FOUND:
+            console.log('No se encontró informacion')
+            break
+    }
+    
+}
+
+const draw = data => {
+    const container = document.createElement('div')
+    data.forEach(comic => {
+        const comicHTML = `
+        <div>
+            <h2>${comic.name}</h2>
+            <img src="${comic.thumbnail.path}/portrait_incredible.${comic.thumbnail.extension}" alt="$comic.name">
+        </div>`
+        container.insertAdjacentHTML('beforeend', comicHTML);
+    });
+
+    myContent.appendChild(container);
+}
